dca0e109f2b42fa6bda0d3965f40ce4851080426
songhaomiao <songhmf@yonyou.com>
2020-10-27 16:04:36 +0800

    -feat:week2

--- Week_02/lesson4_question2_nio/src/main/java/OkHttpTest.java
+++ Week_02/lesson4_question2_nio/src/main/java/OkHttpTest.java
@@ -1,8 +1,22 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.OkHttpClient;
 
 /**
  * @author haomiaosong
  * @date 2020/10/27 3:53 PM
  */
 public class OkHttpTest {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder().url("http://localhost:8808/test").build();
+            Response response = client.newCall(request).execute();
+            byte[] bytes = response.body().bytes();
+            System.out.println(new String(bytes, "UTF-8"));
+        } finally {
+            client.clone();
+        }
+    }
 }
