368d1093bfe6a84bee14023f89cb0e736daa532a
songhaomiao <songhmf@yonyou.com>
2020-10-27 16:02:52 +0800

    -feat:提交第二周作业

---
+++ Week_02/lesson3_question1_gcanalysis/src/main/java/GcLogAnalysis.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * @author haomiaosong
+ * @date 2020/10/27 9:38 AM
+ */
+public class GcLogAnalysis {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        long startMillis = System.currentTimeMillis();
+
+        long timeoutMillis = TimeUnit.SECONDS.toMillis(1);
+        long endMillis = startMillis + timeoutMillis;
+        LongAdder counter = new LongAdder();
+        System.out.println("开始创建对象...");
+        int cacheSize = 2000;
+        Object[] cachedGarbage = new Object[cacheSize];
+        while (System.currentTimeMillis() < endMillis) {
+            Object garbage = generateGarbage(100*1024);
+            counter.increment();
+            int randomIndex = random.nextInt(2 * cacheSize);
+            if (randomIndex < cacheSize) {
+                cachedGarbage[randomIndex] = garbage;
+            }
+        }
+        System.out.println("ִ当前创建对象数量:" + counter.longValue());
+    }
+
+
+    private static Object generateGarbage(int max) {
+        int randomSize = random.nextInt(max);
+        int type = randomSize % 4;
+        Object result = null;
+        switch (type) {
+            case 0:
+                result = new int[randomSize];
+                break;
+            case 1:
+                result = new byte[randomSize];
+                break;
+            case 2:
+                result = new double[randomSize];
+                break;
+            default:
+                StringBuilder builder = new StringBuilder();
+                String randomString = "randomString-Anything";
+                while (builder.length() < randomSize) {
+                    builder.append(randomString);
+                    builder.append(max);
+                    builder.append(randomSize);
+                }
+                result = builder.toString();
+                break;
+        }
+        return result;
+    }
+}
---
+++ Week_02/lesson4_question2_nio/src/main/java/OkHttpTest.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author haomiaosong
+ * @date 2020/10/27 3:53 PM
+ */
+public class OkHttpTest {
+}
